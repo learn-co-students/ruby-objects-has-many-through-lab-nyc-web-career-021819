09aae624e2b86ece2e2d04e897a13b0b1739c05f
Nathaniel Cain <nathaniel2897@gmail.com>
2019-02-25 18:38:54 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  @@all = []
+
+  attr_reader :name, :appointment, :patient
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+
+
+end
